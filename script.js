c6416fb485c1d0c4848b8cd0828ed69950c5f7c4
Prohor <prohor-yakovlev@mail.ru>
2022-03-28 16:19:01 +0300

    eventlistener

---
+++ script.js
@@ -0,0 +1,41 @@
+"use strict";
+const btns = document.querySelectorAll('button'),
+      overlay = document.querySelector('.overlay');
+
+// btn.onclick = function() {
+//   alert('Click');
+// };
+
+// btn.onclick = function() {
+//   alert('Second Click');
+// };
+
+// btn.addEventListener('click', () => {
+//   alert('Click');
+// });
+
+
+// let i = 0;
+
+const deleteElement = (e) => {
+  console.log(e.target);
+  console.log(e.type);
+  // i++;
+//   if (i == 1) {
+//     btn.removeEventListener('click', deleteElement);
+//   }
+};
+
+// btn.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
+
+btns.forEach(btn => {
+  btn.addEventListener('click', deleteElement, {once: true});
+});
+
+const link = document.querySelector('a');
+
+link.addEventListener('click', function(event) {
+  event.preventDefault();
+  console.log(event.target);
+});
