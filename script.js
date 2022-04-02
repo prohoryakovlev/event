eaf0fb0d157009db4a3e8789e76d526ccaf6e3b6
Prohor <prohor-yakovlev@mail.ru>
2022-04-02 21:15:33 +0300

    child

--- script.js
+++ script.js
@@ -1,41 +1,11 @@
 "use strict";
-const btns = document.querySelectorAll('button'),
-      overlay = document.querySelector('.overlay');
 
-// btn.onclick = function() {
-//   alert('Click');
-// };
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
 
-// btn.onclick = function() {
-//   alert('Second Click');
-// };
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
 
-// btn.addEventListener('click', () => {
-//   alert('Click');
-// });
+console.log(document.querySelector('#current').parentNode.parentNode);
 
-
-// let i = 0;
-
-const deleteElement = (e) => {
-  console.log(e.target);
-  console.log(e.type);
-  // i++;
-//   if (i == 1) {
-//     btn.removeEventListener('click', deleteElement);
-//   }
-};
-
-// btn.addEventListener('click', deleteElement);
-// overlay.addEventListener('click', deleteElement);
-
-btns.forEach(btn => {
-  btn.addEventListener('click', deleteElement, {once: true});
-});
-
-const link = document.querySelector('a');
-
-link.addEventListener('click', function(event) {
-  event.preventDefault();
-  console.log(event.target);
-});
